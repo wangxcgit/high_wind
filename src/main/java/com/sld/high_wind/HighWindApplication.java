821554e1c572b8752d86a3c11a02df8a2fea9d7b
liuguang91 <8921886+liuguang91@user.noreply.gitee.com>
2024-03-13 23:50:00 +0800

    新建仓库

---
+++ src/main/java/com/sld/high_wind/HighWindApplication.java
@@ -0,0 +1,13 @@
+package com.sld.high_wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighWindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighWindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sld/high_wind/HighWindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sld.high_wind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighWindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
